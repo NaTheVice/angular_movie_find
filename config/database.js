98b2627a4bdea5c7812daa4760b771219c4dd8a3
NaTheVice <shuth@web.de>
2018-02-10 16:28:33 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
 module.exports = {
  
-  //database: 'mongodb://root2:root2@ds151941.mlab.com:51941/mongo-bongo',    //dev
-  //database: 'mongodb://mongodb://localhost:27017/ng-movies',
-  database: 'mongodb://root:root@ds013619.mlab.com:13619/movie-master',
+  database: 'mongodb://USERNAME:PASSWORD@YOURDB.mlab.com:13619/YOUR_APP_NAME',
   secret: 'yoursecret'
 }
